c01e6e0a75cc970f759d9b4e64ac92a4cfb38caa
Aman Kumar Singh <singh123aman12345@gmail.com>
2024-07-10 17:05:55 +0530

    create pages and added route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/signin",
+        element: <SignIn />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+      {
+        path:'/profile',
+        element:<Profile />
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className='bg-slate-500'>App</div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
